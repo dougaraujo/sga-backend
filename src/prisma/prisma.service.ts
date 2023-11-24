d8e59db7cca2753e3b14d42009c18d17b5e1c63d
Douglas Araújo <devdoug45@gmail.com>
2023-11-24 00:32:26 +0000

    fix: injeta o ConfigService no prisma.service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor(private configService: ConfigService) {
+  constructor(private configService: ConfigService = new ConfigService()) {
     super({
       datasources: {
         db: {
