18234b0c23021901fd3087c3acbea237a392b422
Douglas Araújo <devdoug45@gmail.com>
2023-11-24 00:30:43 +0000

    config: implementa o ConfigModule do Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { AuthModule } from './auth/auth.module';
 import { ActivityModule } from './activity/activity.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, ActivityModule, PrismaModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    ActivityModule,
+    PrismaModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
